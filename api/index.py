310f8c6c09f50d6deeb40d8f5d258ab63fe85958
Ell0203 <69006643+Ell0203@users.noreply.github.com>
2026-02-25 00:57:51 +0900

    Delete api directory

--- api/index.py
+++
@@ -1,8 +0,0 @@
-import sys
-import os
-
-# 현재 api 폴더의 상위 폴더(루트 경로)를 파이썬 경로에 추가합니다.
-sys.path.insert(0, os.path.abspath(os.path.join(os.path.dirname(__file__), '..')))
-
-# 루트 경로에 있는 app.py 안의 Flask 인스턴스(app)를 가져옵니다.
-from app import app
